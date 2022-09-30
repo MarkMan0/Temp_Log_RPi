c92b085861fcf5d033d5ba72f3b54f82e64237d8
MarkMan0 <pal.mark28@gmail.com>
2022-09-30 18:31:47 +0200

    save to DB in UTC time

--- capture_data.py
+++ capture_data.py
@@ -14,11 +14,11 @@ def log_DHT(sensor_id: int, temp: float, hum: float) -> bool:
         
         if _verify_value(temp, -10, 50):
             curs.execute(
-                """INSERT INTO temperature VALUES(datetime(CURRENT_TIMESTAMP, 'localtime'), (?), (?))""",
+                """INSERT INTO temperature VALUES(datetime(CURRENT_TIMESTAMP, 'utc'), (?), (?))""",
                 (temp, sensor_id))
         if _verify_value(hum, 0, 100):
             curs.execute(
-                """INSERT INTO humidity VALUES(datetime(CURRENT_TIMESTAMP, 'localtime'), (?), (?))""",
+                """INSERT INTO humidity VALUES(datetime(CURRENT_TIMESTAMP, 'utc'), (?), (?))""",
                 (hum, sensor_id))
             
         conn.commit()
